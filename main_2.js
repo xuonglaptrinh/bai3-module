f5fb3907ab9560451c7a7460f9edac5473a3390e
NgaNguyenDuy <kyo1508@gmail.com>
2014-08-30 00:03:13 +0700

    delete file

--- main_2.js
+++
@@ -1,8 +0,0 @@
-//main_2.js
-var MyModule = require('./my_module_2');
-
-console.log(MyModule.myObject);
-console.log(MyModule.add(1,2));
-console.log(MyModule.sub(5,3));
-
-
--- my_module_2.js
+++
@@ -1,9 +0,0 @@
-//my_module_2.js
-
-var myModule = {
-    add: function(a, b) { return (a+b); },
-    sub: function(a, b) { return (a-b); },
-    myObject: {"name": "Nguyen Van Hung"}
-};
-
-module.exports = myModule;
